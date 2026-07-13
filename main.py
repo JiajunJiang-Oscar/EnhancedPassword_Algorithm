3bbadc08e258c064c7a18e102acf9c8ac07883d1
Oscar Jiang <jiajun.jiang@my.jcu.edu.au>
2026-07-13 16:31:16 +0800

    Problem fixed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from abbreviation import generate_platform_id
 from length_enhancer import enhance_length
 from structure_generator import generate_password
+import sys
 
 def main():
     print("██████╗  █████╗ ███████╗███████╗██╗    ██╗ ██████╗ ██████╗ ██████╗\n" 
@@ -20,36 +21,63 @@ def main():
 
     while True:
 
-        print("MENU\n"
-              "1. Generate New Password\n"
-              "2. View Password History\n"
-              "3. Exit"
+        print("\nMENU\n"
+              "1. Check the introduction and rules\n"
+              "2. Generate New Password\n"
+              "3. View Password History\n"
+              "E. Exit"
               )
-        choice = input("Select: ")
+        choice = input("Select: ").lower()
 
         if choice == "1":
-            generate_new_password()
+            rule_display()
         elif choice == "2":
-            print("Coming soon...")
+            generate_new_password()
         elif choice == "3":
-            print("Goodbye!")
+            print("Coming soon...")
+        elif choice == "e":
+            print("\nGoodbye!")
             break
 
         else:
             print("\n! Invalid option !\n")
 
+def rule_display():
+    print("This Password Enhancement platform is aim to create strong and easy to memory password by our own algorithm.\n "
+          "\nWhen user try to create a new password, the platform will request user to give below information:\n"
+          "The User Name (can be real name)\n"
+          "The Name of The Registered Platform (The entire process of the input platform will automatically become an abbreviation)\n"
+          "A Year That Leaves A Deep Impression (Recommendation does not use  birthdays but the year when registered)\n "
+          "\nAt the same time, the platform will remember all password that created when the platform is running until stop")
+    back_or_exit()
+
 def generate_new_password():
     # Remove the spaces entered by the user when entering the name
-    name = "".join(input("\nEnter your account name: ").split())
-    platform = input("Enter the name of the platform you register for: ").strip()
-    anchor = input("Enter memory anchor (e.g. the year you register in): ").strip()
+    name = "".join(input("\n================================\nEnter The Name: ").split())
+    platform = input("Enter Registered Platform Name: ").strip()
+    anchor = input("Enter Deep Impression Year ").strip()
 
     platform_id = generate_platform_id(platform)
     elements = [name, platform_id, anchor]
     enhanced_elements = enhance_length(elements)
 
     password = generate_password(enhanced_elements)
-    print(f"\nYour new strong password is: {password}\n")
+    print(f"\n================================\nYour new strong password is: {password}")
+    back_or_exit()
+
+
+def back_or_exit():
+
+    while True:
+        choice = input("\n================================\n[B] Back to Menu | [E] Exit : ").strip().lower()
+
+        if choice == "b":
+            return
+        elif choice == "e":
+            print("\nGoodbye!")
+            sys.exit()
+        else:
+            print("Invalid input. Please enter B or E.")
 
 if __name__ == "__main__":
     main()
